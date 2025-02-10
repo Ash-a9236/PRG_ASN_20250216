d18e0dd7886c4b92ba5f121d4a4b21915679c086
Loading... <158498496+Ash-a9236@users.noreply.github.com>
2025-02-10 12:37:00 -0500

    creation of Enum severity + implementation of the base constructor of patient class

---
+++ src/main/java/asha9236/example/Patient.java
@@ -0,0 +1,18 @@
+package asha9236.example;
+
+public class Patient {
+    private String name;
+    private int age;
+    private SEVERITY severity;
+    private boolean inICU;
+
+    public Patient (String name, int age, SEVERITY severity, boolean inICU) {
+        this.name = name;
+        this.age = age;
+        this.severity = severity;
+    }
+
+
+
+
+}
---
+++ src/main/java/asha9236/example/SEVERITY.java
@@ -0,0 +1,5 @@
+package asha9236.example;
+
+public enum SEVERITY {
+    LOW, MEDIUM, HIGH, CRITICAL
+}
